4e7e44c8b7c508e0c416ba9f23e41b4c62fdd3d8
hsiehchenwei <hsiehchenwei@gmail.com>
2023-06-28 10:56:40 +0800

    fix

--- Runtime/UIToolkit/BtnPageSystem/UI_BTNLancherBase.cs
+++ Runtime/UIToolkit/BtnPageSystem/UI_BTNLancherBase.cs
@@ -32,7 +32,7 @@ namespace Cameo.UI
     public class UI_BTNLancherBase : MonoBehaviour
     {
         public UnityAction<ScoreResult> OnMissionDone;
-
+        protected bool IsFirstPlay=true;
         public UnityAction OnMissionCancel;
         /// <summary>
         /// override to lanch any by BTNPage
@@ -40,6 +40,7 @@ namespace Cameo.UI
         /// <returns></returns>
         public virtual IEnumerator LanchProcess(BTNData BTNData, UnityAction<ScoreResult> _OnMissionDone,string DataSheetID, bool IsFirstPlay, UnityAction _OnMissionCancel = null)
         {
+            this.IsFirstPlay=IsFirstPlay;
             OnMissionDone = _OnMissionDone;
             OnMissionCancel = _OnMissionCancel;
             yield return null;
